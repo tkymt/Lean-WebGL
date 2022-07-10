5f579001dadb0162a05262c1ff18eb77a31246d5
Taku Yamamoto <jts0ymt@outlook.jp>
2022-07-10 21:30:38 +0900

    頂点シェーダーのプログラムを書いた

--- webgl-demo.js
+++ webgl-demo.js
@@ -19,4 +19,18 @@ function main(){
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // 指定されたクリアカラーでカラーバッファーをクリアします
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+    // 頂点シェーダーのプログラム
+    const vsSource = `
+    attribute vec4 aVertexPosition;
+
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main(){
+        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+
+    }
+    `;
 }
