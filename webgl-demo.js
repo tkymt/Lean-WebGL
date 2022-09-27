65f721ee1f9c05a5f96172b6399301f3c2c555de
Taku Yamamoto <jts0ymt@outlook.jp>
2022-09-28 01:32:14 +0900

    シェーダーの初期化を行うコードを書いた

--- webgl-demo.js
+++ webgl-demo.js
@@ -40,4 +40,54 @@ function main(){
             gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
         }
     `;
+
+    // シェーダーのプログラムを初期化する
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+    
+
+}
+
+// シェーダープログラムを初期化して、WebGLがデータの描画方法を認識できるようにする
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    // シェーダープログラムを作成する
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    // もしシェーダープログラムを作成するのに失敗したらアラートを出力する
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert(
+            "シェーダープログラムを初期化できません：" +
+            gl.getProgramInfoLog(shaderProgram)
+        );
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+// 指定されたタイプのシェーダーを作成する。ソースのアップロードとコンパイルをする。
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    
+    // ソースをシェーダーオブジェクトに送信する
+    gl.shaderSource(shader, source);
+
+    // シェーダープログラムをコンパイルする
+    gl.compileShader(shader);
+
+    // 正常にコンパイルされたかどうかを確認する
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(
+            "シェーダーのコンパイル中にエラーが発生しました：" + gl.getShaderInfoLog(shader)
+        );
+        gl.deleteShader(shader);
+    return null;
+    }
+
+    return shader;
 }
