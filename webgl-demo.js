cd9f6a84d4915eab71721d081e88a9870c1e6998
Taku Yamamoto <jts0ymt@outlook.jp>
2022-09-28 00:58:15 +0900

    フラグメントシェーダーのプログラムを書いた

--- webgl-demo.js
+++ webgl-demo.js
@@ -23,14 +23,21 @@ function main(){
 
     // 頂点シェーダーのプログラム
     const vsSource = `
-    attribute vec4 aVertexPosition;
+        attribute vec4 aVertexPosition;
 
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
+        uniform mat4 uModelViewMatrix;
+        uniform mat4 uProjectionMatrix;
 
-    void main(){
-        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+        void main(){
+           gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
 
-    }
+        }
+    `;
+
+    // フラグメントシェーダーのプログラム
+    const fsSource = `
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
     `;
 }
